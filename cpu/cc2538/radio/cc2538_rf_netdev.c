e2e9ad94f2e58a68a779d4c21d883897925a7e90
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-04-16 15:26:37 +0100

    cpu/cc2538: Prevent underflow of the RX FIFO

--- cpu/cc2538/radio/cc2538_rf_netdev.c
+++ cpu/cc2538/radio/cc2538_rf_netdev.c
@@ -299,13 +299,28 @@ static int _recv(netdev_t *netdev, void *buf, size_t len, void *info)
     size_t pkt_len;
 
     if (buf == NULL) {
+        /* Check that the last byte of a new frame has been received */
+        if(RFCORE->XREG_FSMSTAT1bits.FIFOP == 0) {
+            DEBUG_PRINT("cc2538_rf: ERROR FIFOP == 0\n");
+            return -EAGAIN;
+        }
+
         /* GNRC wants to know how much data we've got for it */
         pkt_len = rfcore_read_byte();
 
         /* Make sure pkt_len is sane */
         if (pkt_len > CC2538_RF_MAX_DATA_LEN) {
+            DEBUG_PRINT("cc2538_rf: ERROR pkt_len > CC2538_RF_MAX_DATA_LEN\n");
             RFCORE_SFR_RFST = ISFLUSHRX;
             return -EOVERFLOW;
+        }
+
+        /* Make sure pkt_len is not too short.
+         * There are at least 2 bytes (FCS). */
+        if (pkt_len < IEEE802154_FCS_LEN) {
+            DEBUG_PRINT("cc2538_rf: ERROR pkt_len < IEEE802154_FCS_LEN\n");
+            RFCORE_SFR_RFST = ISFLUSHRX;
+            return -ENODATA;
         }
 
         /* CRC check */
@@ -329,19 +344,24 @@ static int _recv(netdev_t *netdev, void *buf, size_t len, void *info)
 
     rfcore_read_fifo(buf, pkt_len);
 
-    if (info != NULL && RFCORE->XREG_RSSISTATbits.RSSI_VALID) {
-        uint8_t corr_val;
-        int8_t rssi_val;
-        netdev_ieee802154_rx_info_t *radio_info = info;
-        rssi_val = rfcore_read_byte() + CC2538_RSSI_OFFSET;
+    int8_t rssi_val = rfcore_read_byte();
+    uint8_t crc_corr_val = rfcore_read_byte();
 
-        RFCORE_ASSERT(rssi_val > CC2538_RF_SENSITIVITY);
+    /* CRC check passed earlier, it still should */
+    RFCORE_ASSERT(crc_corr_val & CC2538_CRC_BIT_MASK);
+
+    if (info != NULL) {
+        netdev_ieee802154_rx_info_t *radio_info = info;
+
+        /* The RSSI included in the FCS should always be valid here */
+        RFCORE_ASSERT(RFCORE->XREG_RSSISTATbits.RSSI_VALID);
+        //RFCORE_ASSERT(rssi_val > CC2538_RF_SENSITIVITY);
 
         /* The number of dB above maximum sensitivity detected for the
          * received packet */
-        radio_info->rssi = -CC2538_RF_SENSITIVITY + rssi_val;
+        radio_info->rssi = -CC2538_RF_SENSITIVITY + rssi_val + CC2538_RSSI_OFFSET;
 
-        corr_val = rfcore_read_byte() & CC2538_CORR_VAL_MASK;
+        uint8_t corr_val = crc_corr_val & CC2538_CORR_VAL_MASK;
 
         if (corr_val < CC2538_CORR_VAL_MIN) {
             corr_val = CC2538_CORR_VAL_MIN;
@@ -356,7 +376,13 @@ static int _recv(netdev_t *netdev, void *buf, size_t len, void *info)
                           (CC2538_CORR_VAL_MAX - CC2538_CORR_VAL_MIN);
     }
 
-    RFCORE_SFR_RFST = ISFLUSHRX;
+    /* Check for overflow of the rx fifo */
+    if(RFCORE->XREG_FSMSTAT1bits.FIFOP != 0 &&
+       RFCORE->XREG_FSMSTAT1bits.FIFO == 0)
+    {
+        DEBUG_PRINT("cc2538_rf: RXFIFO Overflow\n");
+        RFCORE_SFR_RFST = ISFLUSHRX;
+    }
 
     return pkt_len;
 }
